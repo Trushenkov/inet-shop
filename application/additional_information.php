39078e6f8fda81551eb47853c5c3144e88b956f5
Trushenkov Dmitry <diman28051999@gmail.com>
2018-10-17 02:14:04 +0300

    Added block additional information.

---
+++ application/additional_information.php
@@ -0,0 +1,19 @@
+<img id="img-logo" src="../css/images/logo.jpg">
+<!-- Информационный блок -->
+<div id="personal-info">
+    <p>Звонок бесплатный</p>
+    <h3>8 (800) 888-70-07</h3>
+    <img src="../css/images/phone.png">
+    <p>Режим работы: </p>
+    <p>Будние дни: с 9:00 до 18:00</p>
+    <p>Суббота, Воскресенье - Выходные</p>
+    <img src="../css/images/time.png">
+</div>
+<!-- Форма поиска -->
+<!--<div id="block-search">-->
+<!--    <form method="GET" action="search.php?q=">-->
+<!--        <img src="../css/images/search.png">-->
+<!--        <input type="text" id="input-search" name="q" placeholder="Поиск среди более 100 000 товаров">-->
+<!--        <input type="submit" id="button-search" value="Поиск">-->
+<!--    </form>-->
+<!--</div>-->
