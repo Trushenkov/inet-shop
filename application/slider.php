f9560f9e26b948d0e7d08122d84ce8ad24065013
Trushenkov Dmitry <diman28051999@gmail.com>
2018-10-18 01:37:37 +0300

    Created slider for home-page

---
+++ application/slider.php
@@ -0,0 +1,30 @@
+<!-- Content Slider -->
+<div id="slider" class="box">
+    <div id="slider-holder">
+        <ul>
+            <?php
+            $result = mysqli_query($link, "SELECT * FROM `products` WHERE `slide` = 1");
+            $index = 0;
+            if (mysqli_num_rows($result) > 0) {
+                // Выбирает одну строку из результирующего набора и помещает ее в ассоциативный массив.
+                $rows = mysqli_fetch_array($result);
+                do {
+                    echo '
+            <li><a href="store.php?id=' . $rows["id"] . '"><img src="css/images/products/' . $rows["image"] . '-slide.jpg"</a>';
+                    $index++;
+                } while ($rows = mysqli_fetch_array($result));
+            }
+            ?>
+        </ul>
+
+    </div>
+    <div id="slider-nav">
+        <?php
+        for ($i = 1; $i < $index; $i++) {
+            echo '
+            <a href="#">' . $i . '</a>';
+        }
+        ?>
+    </div>
+</div>
+<!--End Content Slider-->
