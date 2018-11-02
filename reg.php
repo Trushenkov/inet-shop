0a630efbfcda0425583989f9c6d7cda28105bfea
Trushenkov Dmitry <diman28051999@gmail.com>
2018-11-02 21:03:40 +0300

    created page "reg.php"

---
+++ reg.php
@@ -0,0 +1,98 @@
+<?php
+session_start();
+require_once("functions/users.php");
+$value_page = 3;
+include("functions/db_connect.php");
+if (isset($_POST['reg'])) {
+    $username = htmlspecialchars($_POST['username']);
+    $email = htmlspecialchars($_POST['email']);
+    $password = htmlspecialchars($_POST['password']);
+    $bad = false;
+    session_start();
+    unset($_SESSION['error_username']);
+    unset($_SESSION['error_email']);
+    unset($_SESSION['error_password']);
+    unset($_SESSION["reg_success"]);
+
+//    if (strlen($username) < 3 || strlen($username) > 20) {
+//        $_SESSION['error_username'] = 1;
+//        $bad = true;
+//    }
+//    if (strlen($email) < 3 || strlen($email) > 32) {
+//        $_SESSION['error_email'] = 1;
+//        $bad = true;
+//    }
+//    if (strlen($password) < 6 || strlen($password) > 32) {
+//        $_SESSION['error_password'] = 1;
+//        $bad = true;
+//    }
+
+    if (checkLoginUser($username) > 0){
+//        echo    "<p style='color: red; font-size:14px; margin-left: 36%; margin-top:0px; margin-bottom: 10px; display: inline-block;'>
+//                    Этот логин  уже занят! Введите, пожалуйста, другой  логин
+//                 </p>";
+        $_SESSION['error_username'] = 1;
+        $bad = true;
+    }
+
+    if (checkEmailUser($email) > 0){
+//        echo    "<p style='color: red; font-size:14px; margin-left: 36%; margin-top: 0px; margin-bottom: 20px; display: inline-block;'>
+//                    Пользователь с таким email уже существует. Введите, пожалуйста, другой email
+//                 </p>";
+        $_SESSION['error_email'] = 1;
+        $bad = true;
+    }
+
+    if (!validatePassword($password)){
+//        echo    "<p style='color: red; font-size:14px; margin-left: 36%; margin-top: 0px; margin-bottom: 20px; display: inline-block;'>
+//                    Извините, длина пароля должна быть от 3 до 20 символов. Придумайте другой пароль
+//                 </p>";
+        $_SESSION['error_password'] = 1;
+        $bad = true;
+    }
+
+    if (!$bad) {
+        regUser($username, $email, $password);
+        $_SESSION["reg_success"] = 1;
+        header("Location: user_panel.php");
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Регистрация</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+<div class="shell">
+    <?php
+    include("application/header.php");
+    if ($_SESSION["error_username"] == 1) echo '<p style="text-align: center;  margin-top: 30px;"><span style="color: red">Этот логин  уже занят! Введите, пожалуйста, другой  логин</p>';
+    if ($_SESSION["error_email"] == 1) echo '<p style="text-align: center;  margin-top: 30px;"><span style="color: red">Пользователь с таким email уже существует. Введите, пожалуйста, другой email</span></p>';
+    if ($_SESSION["error_password"] == 1) echo '<p style="text-align: center;  margin-top: 30px;"><span style="color: red">Извините, длина пароля должна быть от 3 до 20 символов. Придумайте другой пароль</span></p>';
+    echo '
+        <div id="main">
+            <div class="form">
+                <form id="login" style="height: 270px; " name="auth" action="" method="post">
+                    <h1 class="h1">Регистрация</h1>
+                    <fieldset id="inputs">
+                        <input id="username" name="username" type="text" placeholder="Имя пользователя" autofocus required>
+                        <input id="username" name="email" type="email" placeholder="E-mail" autofocus required>
+                        <input id="password" name="password" type="password" placeholder="Пароль" required>
+                    </fieldset>
+                    <fieldset id="actions">
+                        <input type="submit" name="reg" id="submit" style="width: 230px" value="ЗАРЕГИСТРИРОВАТЬСЯ">
+                        <a href="user_panel.php">Авторизация</a>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+        ';
+    include("application/footer.php");
+    ?>
+</div>
+</body>
+</html>
---
+++ user_panel.php
@@ -0,0 +1,78 @@
+<?php
+$value_page = 3;
+session_start();
+include("functions/db_connect.php");
+require_once("functions/users.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="css/ie6.css" type="text/css">
+</head>
+<body>
+<div class="shell">
+    <?php
+    include("application/header.php");
+    if ($_SESSION["error_auth"] == 1) {
+        echo '<p style="text-align: center; margin-top: 30px;"><span style="color: red;">Неверное имя пользователя и/или пароль!</span></p>';
+        unset($_SESSION["error_auth"]);
+    }
+    if (checkUser($_SESSION["username"], $_SESSION["password"])) {
+        echo '
+         <div id="main">
+            <div class="user-position">
+                <div class="auth">
+                    <p>Здравствуйте, <span>' . $_SESSION['username'] . '</span></p>
+                    <p><a href="logout.php" title="Выход">Выход</a></p>     
+                </div>
+                <p>Спасибо за авторизацию на сайте нашего интернет магазина продажи мебели с одноименным названием <span>Интернет – магазин мебели для дома</span></p>
+                
+                <h1>Ваша контактная информация:</h1>
+                <br>
+                <hr>
+                <h2>Имя пользователя: <span>' . $_SESSION['username'] . '</span></h2> 
+                <h2>Электронная почта: <span>' . getEMAIL($_SESSION['username']) . '</span></h2>
+                <br>
+                <hr> 
+                <p>Никому не сообщайте свой пароль в плане безопасности!</p>
+                ';
+        if (getPscCart($link, getID($_SESSION['username'])) > 0) {
+            echo '<h1><span>У вас всё ещё остались товар(ы) в корзине!</span></h1>
+                    <p>Очистите корзину или приобретите товар(ы). Для этого перейдите в <a href="cart.php?action=oneclick   ">корзину</a></p>
+            ';
+        } else {
+            echo '<h1><span>Ваша корзина пуста! Купите товар на ваш вкус!</span></h1>';
+        }
+        echo '
+            <br>
+            </div>
+        </div>';
+    } else {
+        echo '
+        <div id = "main">
+            <div class="form">
+                <form id = "login" action = "login.php" method = "post">
+                    <h1 class="h1"> Авторизация</h1>
+                    <fieldset id = "inputs">
+                        <input id = "username" name = "username" type = "text" placeholder = "Имя пользователя" autofocus required>
+                        <input id = "password" name = "password" type = "password" placeholder = "Пароль" required>
+                    </fieldset>
+                    <fieldset id = "actions">
+                        <input type = "submit" id = "submit" value = "ВОЙТИ">
+                        <a href = "reg.php">Регистрация</a>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+    ';
+    }
+    include("application/footer.php");
+    ?>
+</div>
+</body>
+</html>
+
+
